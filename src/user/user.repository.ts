8924df07c071ae8eaa6b2a21aa6fb6aa21be8ab4
Misha <misha1235000@gmail.com>
2018-08-09 10:59:41 +0300

    Added user repository with promise functions

---
+++ src/user/user.repository.ts
@@ -0,0 +1,21 @@
+import { UserModel } from './user.model';
+import { IUser } from './user.interface';
+
+export class UserRepository {
+
+    public static findById(id: string): Promise<IUser | null> {
+        return UserModel.findOne({ _id: id }).exec();
+    }
+
+    public static create(user: IUser): Promise<IUser> {
+        return UserModel.create(user);
+    }
+
+    public static update(id: string, user: Partial<IUser>): Promise<IUser | null> {
+        return UserModel.findByIdAndUpdate(id, user, { new: true, runValidators: true }).exec();
+    }
+
+    public static delete(id: string): Promise<IUser | null> {
+        return UserModel.findByIdAndRemove(id).exec();
+    }
+}
