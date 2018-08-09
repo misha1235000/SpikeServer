aaa6c87df65887293b299ac59dc267e15ae1794f
Misha <misha1235000@gmail.com>
2018-08-09 11:00:39 +0300

    Added new user validator

---
+++ src/user/user.validator.ts
@@ -0,0 +1,34 @@
+import { Types } from "mongoose";
+import { IUser } from "./user.interface";
+
+export class UserValidator {
+
+    private static readonly maxTitleLength = 256;
+
+    static isValid(user: IUser): boolean {
+        return user && UserValidator.isUsernameValid(user.name) &&
+               UserValidator.isHostnameValid(user.hostname) &&
+               UserValidator.isCallbackValid(user.callback) &&
+               UserValidator.isPasswordValid(user.password);
+    }
+
+    static isUsernameValid(username: string): boolean {
+        const usernameRegex: RegExp = /[A-z0-9]{4, 15}/m;
+        return usernameRegex.test(username);
+    }
+
+    static isHostnameValid(hostname: string): boolean {
+        const hostnameRegex: RegExp = /(?:http|https):\/\/[A-z0-9]+/m;
+        return hostnameRegex.test(hostname);
+    }
+
+    static isCallbackValid(callback: string): boolean {
+        const callbackRegex: RegExp = /(?:http|https):\/\/[A-z0-9]+\/[A-z0-9]+/m;
+        return callbackRegex.test(callback);
+    }
+
+    static isPasswordValid(password: string): boolean {
+        const passwordRegex: RegExp = /[A-z0-9!@#$%^&*()_-=+]/m;
+        return passwordRegex.test(password);
+    }
+}
