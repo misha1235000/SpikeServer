1e872062472778d08171671d7ca525ba851e5192
Misha <misha1235000@gmail.com>
2018-08-09 11:00:58 +0300

    Added promise wrapper

---
+++ src/utils/warpper.ts
@@ -0,0 +1,9 @@
+import {Response, Request, NextFunction} from 'express';
+
+export class Wrapper {
+    static wrapAsync(func: any) {
+        return (req: Request, res: Response, next: NextFunction ) => {
+            func(req, res, next).catch(next);
+        }
+    }
+}
