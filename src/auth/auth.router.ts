3069e3133dccca3d03db7ed1acca9f68c2fe3df9
Misha <misha1235000@gmail.com>
2018-08-12 14:54:47 +0300

    Added routes for login and logout

--- src/auth/auth.router.ts
+++ src/auth/auth.router.ts
@@ -1,22 +1,16 @@
 import { Router } from 'express';
-import { Request, Response, NextFunction } from 'express';
-import { UserController } from '../user/user.controller';
+import { } from './auth.token';
 import { Wrapper } from '../utils/warpper';
 import { AuthController } from './auth.controller';
-import { IUser } from '../user/user.interface';
 
 export class AuthRouter {
-
     get router() {
         const router: Router = Router();
-        router.use((user: IUser, req: Request, res: Response, next: NextFunction) => {
-            res.status(200).send(user);
-        });
-        router.post('/register', Wrapper.wrapAsync(AuthController.register));
         router.get('/', Wrapper.wrapAsync(AuthController.authorize));
-        /*router.get('/:id', Wrapper.wrapAsync(UserController.findById));
-        router.put('/:id', Wrapper.wrapAsync(UserController.update));
-        router.post('/', Wrapper.wrapAsync(UserController.create));
+        router.post('/register', Wrapper.wrapAsync(AuthController.register));
+        router.post('/login', Wrapper.wrapAsync(AuthController.login));
+        router.get('/logout', Wrapper.wrapAsync(AuthController.logout));
+        /*router.post('/', Wrapper.wrapAsync(UserController.create));
         router.delete('/:id', Wrapper.wrapAsync(UserController.delete));*/
 
         return router;
