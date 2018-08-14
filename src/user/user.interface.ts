97ef7c5ef2f163fb326906e3e598d7535676cded
Misha <misha1235000@gmail.com>
2018-08-14 15:19:51 +0300

    Modified user class to user interface.

--- src/user/user.interface.ts
+++ src/user/user.interface.ts
@@ -1,11 +1,5 @@
-export class IUser {
+export interface IUser {
     _id: string;
     username: string;
     password: string;
-
-    constructor(username: string, password: string, _id: string) {
-        this._id = _id;
-        this.username = username;
-        this.password = password;
-    }
 };
