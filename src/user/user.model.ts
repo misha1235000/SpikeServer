7fec7396de414ec998f9ed2ed4b019f0efea2fec
Shaked Manes <maneshaked@gmail.com>
2018-08-16 21:00:12 +0300

    fix: Adding virtual field in user model for accessing clients of the team Closes #6

--- src/user/user.model.ts
+++ src/user/user.model.ts
@@ -1,9 +1,26 @@
-import { model, Document } from 'mongoose'; 
-import * as mongoose from 'mongoose';
+import { model, Document, Schema } from 'mongoose';
 import { IUser } from './user.interface';
-const UserSchema = new mongoose.Schema({
-  username: {type: String, unique: true},
-  password: String
+
+const UserSchema = new Schema({
+    username: {
+        type: String,
+        unique: true,
+        required: true,
+    },
+    password: {
+        type: String,
+        required: true,
+    },
+});
+
+// Virtual field for getting all the clients of specific team
+// Used via population as described in https://mongoosejs.com/docs/populate.html#populate-virtuals
+UserSchema.virtual('clients', {
+    ref: 'Client',
+    localField: '_id',
+    foreignField: 'teamId',
+    justOne: false,
+    options: { sort: { name: -1 } },
 });
 
 export const UserModel = model<IUser & Document>('User', UserSchema);
