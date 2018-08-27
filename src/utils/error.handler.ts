9f9c3546950d05cd1dc25c5fe7098c2ac1ad7b76
Misha <misha1235000@gmail.com>
2018-08-27 13:14:59 +0300

    fix: Fix error handler function, to handle all kind of errors. Closes #44

--- src/utils/error.handler.ts
+++ src/utils/error.handler.ts
@@ -1,8 +1,12 @@
+// error.handler
+
 import { Request, Response, NextFunction } from 'express';
 import { BaseError } from './error';
 
-export function errorHandler(error: BaseError, req: Request, res: Response, next: NextFunction) {
-    return res.status(error.status).send({
-        message: error.message,
-    });
+export function errorHandler(error: Error, req: Request, res: Response, next: NextFunction) {
+    if (error instanceof BaseError) {
+        return res.status(error.status).send({ message: error.message });
+    }
+
+    return res.status(500).send({ message: error.message });
 }
