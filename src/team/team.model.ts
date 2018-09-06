5d9652b6a1bac5be67e712353389b0bca0b14862
Misha <misha1235000@gmail.com>
2018-09-06 11:04:50 +0300

    fix: Fix password not valid error.

--- src/team/team.model.ts
+++ src/team/team.model.ts
@@ -14,7 +14,6 @@ const TeamSchema = new Schema({
     password: {
         type: String,
         required: true,
-        validate: [TeamValidator.isPasswordValid, 'Password isn\'t valid'],
     },
 });
 
