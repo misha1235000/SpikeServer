253ed51bca4299d0004cf8904e1089d171ca6a4f
Shaked Manes <maneshaked@gmail.com>
2018-09-06 14:53:43 +0300

    refactor: Refactor password hashing when saving new team model or updating, Closes #58

--- src/team/team.model.ts
+++ src/team/team.model.ts
@@ -3,6 +3,7 @@
 import { model, Schema } from 'mongoose';
 import { ITeam } from './team.interface';
 import { TeamValidator } from './team.validator';
+import { hashSync } from 'bcrypt';
 
 const TeamSchema = new Schema({
     teamname: {
@@ -15,6 +16,32 @@ const TeamSchema = new Schema({
         type: String,
         required: true,
     },
+});
+
+/**
+ * Generates hash for password given
+ * @param password - password to hash
+ */
+const generatePasswordHash = (password: string) => {
+    return hashSync(password, 8);
+};
+
+// Before saving, hashing the password saved
+TeamSchema.pre<ITeam>('save', function save() {
+    this.password = generatePasswordHash(this.password);
+});
+
+// Before updating, hashing the updated password (if needed)
+TeamSchema.pre('update', function update(next) {
+
+    // Check if not need to update password
+    const password = this.getUpdate().$set.password;
+    if (!password) {
+        return next();
+    }
+
+    this.getUpdate().$set.password = generatePasswordHash(password);
+    return next();
 });
 
 // Virtual field for getting all the clients of specific team
