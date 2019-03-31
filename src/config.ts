74919965e5baf4084afeaf0acd151b516bac63e9
Misha <misha1235000@gmail.com>
2019-03-31 10:47:53 +0300

    chore: Edit config structure.

--- src/config.ts
+++ src/config.ts
@@ -5,8 +5,8 @@ dotenv.config();
 
 export const config = {
     // Database URLs
-    prodDatabaseUrl: `mongodb://${process.env.DB_PROD_USER}:${process.env.DB_PROD_PASS}@ds125472.mlab.com:25472/${process.env.DB_PROD_NAME}`,
-    testDatabaseUrl: `mongodb://${process.env.DB_TEST_USER}:${process.env.DB_TEST_PASS}@ds123584.mlab.com:23584/${process.env.DB_TEST_NAME}`,
+    prodDatabaseUrl: `mongodb://${process.env.DB_PROD_USER}:${process.env.DB_PROD_PASS}@${process.env.DB_PROD_HOST}/${process.env.DB_PROD_NAME}${process.env.DB_PROD_TYPE || ''}`,
+    testDatabaseUrl: `mongodb://${process.env.DB_TEST_USER}:${process.env.DB_TEST_PASS}@${process.env.DB_TEST_HOST}/${process.env.DB_TEST_NAME}${process.env.DB_TEST_TYPE || ''}`,
 
     // Client Credentials flow configuration for OAuth2
     clientCredentials: {
